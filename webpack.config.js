c74683daf4e791ed3e642b51c3cf080c71011632
Adrian Corcoran <adrian.corcoran@shopify.com>
2018-09-13 09:39:04 +0200

    Show Basic clock - rerender page every time

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + '/src/index.js',
+    mode: 'development',
+    output: {
+        path: __dirname + '/build',
+        filename: 'static/js/[name].[chunkhash:8].js',
+        publicPath: '/'
+    },
+    plugins: [
+        // Generates an `index.html` file with the <script> injected.
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: __dirname + '/index.html'
+        })
+    ]
+};
