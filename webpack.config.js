f06482713385a21f193a94973914b42ce46ef8da
Adrian Corcoran <adrian.corcoran@shopify.com>
2018-09-13 09:44:06 +0200

    Configure webpack to minify js

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: __dirname + '/src/index.js',
-    mode: 'development',
+    mode: process.env.NODE_ENV,
     output: {
         path: __dirname + '/build',
         filename: 'static/js/[name].[chunkhash:8].js',
         publicPath: '/'
+    },
+    module: {
+        rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
     },
     plugins: [
         // Generates an `index.html` file with the <script> injected.
