c3f6969e2c7c5ea51057b01503176246bf5d561d
Adrian Corcoran <adrian.corcoran@shopify.com>
2018-09-13 09:47:48 +0200

    Add production server (different port) and build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const root = document.getElementById('root');
 
 setInterval(() => {
   root.innerHTML = `
+    <p>
+      Mode:
+      <span class=${process.env.NODE_ENV === 'production' ? 'green' : 'red'}>
+        ${process.env.NODE_ENV}
+      </span>
+    </p>
     <p>${new Date().toString()}</p>
     <input placeholder="type something" />
   `;
